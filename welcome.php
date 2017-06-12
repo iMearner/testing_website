310de71205dfeae9b925e17c38c955ca0ec21414
iMearner <201452060@iiitvadodara.ac.in>
2017-06-12 17:05:28 +0530

    Create welcome.php
    earlier config.php was not linked so i temporarily removed it, and i will add it later.

--- welcome.php
+++ welcome.php
@@ -1,11 +1,8 @@
-<?php
-	include('session.php') ;
-?>
 
 <html>
 	<head><title>welcome </title></head>
 	<body>
-		<h1>welcome <?php echo $login_sessio; ?></h1>
+		<h1>welcome <?php echo $_POST["name"]; ?></h1>
 		<h2><a href="logout.php">logout</a></h2>
 	</body>
 </html>
