7ed2d5756e8c1db98db3dab82c3bc3d233365db5
iMearner <201452060@iiitvadodara.ac.in>
2017-06-14 15:20:48 +0530

    now config.php working

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-	define('DB_SERVER', 'localhost:3036');
+	define('DB_SERVER', 'localhost');
 	define('DB_USERNAME', 'root');
 	define('DB_PASSWORD', 'root');
 	define('DB_DATABASE', 'hacktest');
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("connection failed");
 ?>
