5b10f1d58c861aa77a7fde0e412d60b972ead476
iMearner <201452060@iiitvadodara.ac.in>
2017-06-15 21:31:20 +0530

    added query to insert data into database

--- config.php
+++ config.php
@@ -2,5 +2,19 @@
 	define('DB_SERVER', 'localhost');
 	define('DB_USERNAME', 'root');
 	define('DB_PASSWORD', 'root');
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD) or die("connection failed");
+	define('DB_NAME','mydatabase');
+	//checking for connection 
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME) or die("connection failed");
+	if($db){
+		echo "connected.... !";
+	}else{
+		echo "error ";
+	}
+	// creating a rendom query
+	$query = "INSERT INTO mytable(first_name,last_name) VALUES ('abc','sharma');"
+	if(mysqli_query($db,$query)){
+		echo "data updated successfully . ." ;
+	}else{
+		echo "there is a problem while insertion of data. "
+	}
 ?>
