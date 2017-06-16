f6a479cc95224a10bc0b7b2a0f90795917a7cef1
iMearner <201452060@iiitvadodara.ac.in>
2017-06-16 13:18:30 +0530

    data inserted successfully

--- welcome.php
+++ welcome.php
@@ -1,8 +1,26 @@
 
 <html>
 	<head><title>welcome </title></head>
+	
 	<body>
 		<h1>welcome <?php echo $_POST["name"]; ?></h1>
 		<h2><a href="logout.php">logout</a></h2>
+		<?php
+		// connecting with mysql
+		include('config.php');
+		if(isset($_POST['first_name'])){
+			$first_name = $_post['first_name'] ;
+		}
+		if(isset($_POST['last_name'])){
+			$last_name = $_post['last_name'] ;	
+		}
+		$query = "INSERT INTO mytable(first_name,last_name) VALUES('$first_name','$last_name');";
+		if($conn,$query){
+			echo "data updated . . " ;
+		}else{
+			echo "error occured" .mysqli_error() ;
+		}
+		
+		?>
 	</body>
 </html>
