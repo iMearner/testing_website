cbef268aec4d1b11a9a3621d7a7ce2e6e336a944
iMearner <201452060@iiitvadodara.ac.in>
2017-06-20 14:08:14 +0530

    changes made for ease of user
    from login page user can enter any name and one can check if the user exist or not with the inserted data

--- login.php
+++ login.php
@@ -1,11 +1,27 @@
 
 <html>
-	<head>login page </head>
+
+	<head>login page</head>
 	<body>
-		<form action="welcome.php" mathod="post">
-			name : <input type="text" name="name">
-			password : <input type="password" name="password">
-			<input type="submit">
+		<h1>this form is to insert data in database </h1>
+		<form action="welcome.php" method = "post">
+			first name : <input type="text" name="first_name">
+			last name : <input type="text" name = "last_name">
+			<input type="submit" value = "submit">
 		</form>
+		<br>
+		<br>
+		<h1>this form is to check whether inserted name exist or not</h1>
+		<form action="retrive.php" method = "post">
+			first name : <input type="text" name="first_name">
+			<input type="submit" value = "submit">
+		</form>
+<?php 
+
+?>
+
+		<br>
+		<br>
+		<p>to check database connection click <a href="config.php">here</a>.</p>
 	</body>
 </html>
