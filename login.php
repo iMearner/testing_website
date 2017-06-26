95aab03eadaf918a3e7294cd9762df61b9811e1e
iMearner <201452060@iiitvadodara.ac.in>
2017-06-26 23:31:48 +0530

    changed all content
    now there is navigation bar and login fields left in this page

--- login.php
+++ login.php
@@ -43,47 +43,35 @@
 		    background-color: #4CAF50;
 		    color: white;
 		}
+		.login-credintial {
+			margin: 100px ;
+			width: 300px;
+		}
+		.login-credintial input{
+			margin: 10px;
+			border-radius: 5px;
+		}
 	</style>
 
 	
 	<body>
 		<img src="header.png" style="width:100%; height:150px;	">
 		<div class="top-navigation" id="navigation">
-			<a href="#home">Home</a>
+			<a href="login.php">Home</a>
 			<a href="#contact">Contact</a>
 			<a href="#bblog">Blog</a>
 		</div>
-		<!-- creating a drop down menu using java script and little bit of css	 -->
+	 <!-- now create login page that check admin is available in database  -->
+
+	 <div class="login-credintial">
+	 	Username : <input type="text" name="uername"><br>
+	 	Password : <input type="password" name="password"  style="margin-left:12px;"><br>
+	 	<input type="submit" style="float:right; margin-right:50px;">
+	 </div>
+
 		
-		<h1>this form is to insert data in database </h1>
-		<form action="retrive.php" method = "post" >
-			first name : <input type="text" name="first_name">
-			last name : <input type="text" name = "last_name">
-			<input type="submit" value = "submit" name="insert_name">
-		
-		<br>
-		<br>
-		<h1>this form is to check whether inserted name exist or not</h1>
-				check name : <input type="text" name="check_name">
-			<input type="submit" value = "submit_check" name="check_name_submit">
-		</form>
-<?php 
+	
 
-?>
-
-		 <script>
-		// 	function myFunction(){
-		// 		document.getElementById('content').classList.toggle("show") ;
-		// 	}
-		// 	window.onclick = function(event) {
-		// 		if(!event.target.matches('.dropdownbtn')){
-		// 		var temp = document.getElementById('content')
-		// 		temp.className = "dropdown-content";
-		// 	}
-		// 	}
-			
-
-		// </script>
 
 	</body>
 </html>
