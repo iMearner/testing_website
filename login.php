b3612c8dbaa7d85d7a98dd27dd837fd6926876c1
iMearner <201452060@iiitvadodara.ac.in>
2017-06-26 23:59:04 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,22 +3,6 @@
 		body{
 			background-color: #508D6C;
 		}
-		.dropdown-content{
-			display: none ;
-		}
-		.show{
-			display: block;
-		}
-		.b{
-			display: inline-block;
-			float: left;
-			color: black;
-			text-decoration: none;
-			background-color: #508D6C ;
-		}
-		.b : hover{
-			background-color: white ;
-		}
 		.top-navigation {
 			background-color: #333 ;
 			overflow: hidden;
@@ -46,10 +30,29 @@
 		.login-credintial {
 			margin: 100px ;
 			width: 300px;
+			float: left;
 		}
 		.login-credintial input{
 			margin: 10px;
 			border-radius: 5px;
+		}
+		.dropdown{
+			float: right;
+			margin-top: 100px;
+			margin-right: 150px;
+		}
+		.dropdown-content{
+			display: block;
+			background-color: blue;
+		    position: absolute;
+		    background-color: #f9f9f9;
+		    min-width: 80px;
+		    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+		    z-index: 1;
+		}
+		.dropdown-content a {
+			display: inherit;
+
 		}
 	</style>
 
@@ -62,12 +65,28 @@
 			<a href="#bblog">Blog</a>
 		</div>
 	 <!-- now create login page that check admin is available in database  -->
+	 <form  method="post">
+	 	<div class="login-credintial">
+		 	Username : <input type="text" name="uername"><br>
+		 	Password : <input type="password" name="password"  style="margin-left:12px;"><br>
+		 	<input type="submit" style="float:right; margin-right:50px;">
+		 </div>
 
-	 <div class="login-credintial">
-	 	Username : <input type="text" name="uername"><br>
-	 	Password : <input type="password" name="password"  style="margin-left:12px;"><br>
-	 	<input type="submit" style="float:right; margin-right:50px;">
-	 </div>
+		 <div class="dropdown">
+		 	<button>menu</button>
+		 	<div class="dropdown-content">
+		 		<a href="#">test type 1</a>
+		 		<a href="#">test type 2</a>
+		 		<a href="#">test type 3</a>
+		 		<a href="#">test type 4</a>
+		 		<a href="#">test type 5</a>
+		 		<a href="#">test type 6</a>
+		 		<a href="#">test type 7</a>
+		 	</div>
+		 </div>
+	 </form>
+
+	 
 
 		
 	
