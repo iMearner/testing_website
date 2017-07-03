0a9e6d76cb1c1cad631a2dfefd3ad1f271c9ce15
iMearner <201452060@iiitvadodara.ac.in>
2017-07-03 23:35:56 +0530

    Create http-parameter-pollution.php

---
+++ test-cases/http-parameter-pollution.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../style.css">
+	</head>
+
+	
+	<body>
+		<img src="../header.png" style="width:100%; height:150px;	">
+		<div class="top-navigation" id="navigation">
+			<a href="login.php">Home</a>
+			<a href="#contact">Contact</a>
+			<a href="#bblog">Blog</a>
+		</div>
+	 
+		
+
+	</body>
